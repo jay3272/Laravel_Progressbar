dedbe70a2150cbfb0a1a25ee7a1b84bd93f4e0e2
jay3272 <jaywu3272@gmail.com>
2023-05-22 13:03:39 +0800

    初探Blade template, 分切畫面重複使用(@extends, @section, @yield, @include)

--- pb_frontend_assets/resources/views/home.blade.php
+++ pb_frontend_assets/resources/views/home.blade.php
@@ -1,3 +1,15 @@
-<?php   
+<!-- Stored in resources/views/child.blade.php -->
 
-echo "Home Page";
+@extends('layouts.app')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+@parent
+
+<p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+<p>Home Page</p>
+@endsection
---
+++ pb_frontend_assets/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Progressbar Laravel class</title>
+    @include('layouts.meta')
+    @include('layouts.css')
+</head>
+
+<body>
+    @include('layouts.nav')
+    <div class="container">
+        @yield('content')
+    </div>
+    @include('layouts.footer')
+    @include('layouts.js')
+</body>
+
+</html>
